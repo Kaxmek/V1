a045e42da337de7a94ecc8407354b5b631bc30a2
Hussein <118964436+SS7SS@users.noreply.github.com>
2023-02-23 11:43:35 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -221,6 +221,24 @@ def gen_user(choice):
             username = username+'bot'
         else:
             pass
+    if choice == "14":
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(e)
+        f = [c[0], "_", s[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        username = username+'bot'
+        if username in banned[0]:
+            c = random.choices(a)
+            d = random.choices(b)
+            s = random.choices(e)
+            f = [c[0], "_", s[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+            username = username+'bot'
+        else:
+            pass
     if choice == "13":
         c = random.choices(a)
         d = random.choices(b)
